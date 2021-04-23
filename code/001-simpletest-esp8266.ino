6ef06b972941363e0eb500c5db2ac37155f0f5cc
raspberrypisig <raspberrypisig@gmail.com>
2021-04-23 13:11:25 +1000

    Update 001-simpletest-esp8266.ino

--- code/001-simpletest-esp8266.ino
+++ code/001-simpletest-esp8266.ino
@@ -11,27 +11,31 @@
 
 #define SEGMENTS_PER_PANEL 8
 
-#define DEBUG
+//#define DEBUG
 
 void fourChannelShiftOut(uint16_t data1, uint16_t data2, uint16_t data3, uint16_t data4) 
 {  
-    uint16_t mask1 = 0x8000;
+  uint16_t mask = 0x8000;
   
-	for (uint16_t i = 0; i <16 ; i++)  {
-            digitalWrite(PIN_DATA1, (data1 & mask));
-	    digitalWrite(PIN_DATA2, (data2 & mask));
-            digitalWrite(PIN_DATA3, (data3 & mask));
-	    digitalWrite(PIN_DATA4, (data4 & mask));    
-            mask = mask >> 1;
-	    digitalWrite(PIN_CLK, HIGH);
-	    delayMicroseconds(1);
-	    digitalWrite(PIN_CLK, LOW);
-	    #ifdef DEBUG
-	    delay(1000);	
-            #endif
-	}
+  for (uint16_t i = 0; i <16 ; i++)  
+  {
+    digitalWrite(PIN_DATA1, (data1 & mask)?HIGH:LOW);
+    digitalWrite(PIN_DATA2, (data2 & mask)?HIGH:LOW);
+    digitalWrite(PIN_DATA3, (data3 & mask)?HIGH:LOW);
+    digitalWrite(PIN_DATA4, (data4 & mask)?HIGH:LOW);    
+    #ifdef DEBUG
+      Serial.println((data1 & mask)?HIGH:LOW);
+      Serial.println((data2 & mask)?HIGH:LOW);
+      Serial.println((data3 & mask)?HIGH:LOW);
+      Serial.println((data4 & mask)?HIGH:LOW);
+      delay(5000);
+    #endif
+    mask = mask >> 1;
+    digitalWrite(PIN_CLK, HIGH);
+    delayMicroseconds(1);
+    digitalWrite(PIN_CLK, LOW);
+  }
 }
-
 
 void setup()
 {
@@ -54,7 +58,8 @@ void setup()
   uint16_t data3 = 0b0000000000000000;
   uint16_t data4 = 0b1111111111111111;
 
-  for (int i=0; i < SEGMENTS_PER_PANEL; i++) {
+  for (int i=0; i < SEGMENTS_PER_PANEL; i++) 
+  {
     fourChannelShiftOut(data1, data2, data3, data4);  
   }
   
@@ -62,8 +67,6 @@ void setup()
   delayMicroseconds(1);
   digitalWrite(PIN_LATCH, LOW);
   analogWrite(PIN_NOE, 1023/2);
-
-
 }
 
 void loop()
