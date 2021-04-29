55c7167464398a7bfc8af0847d82f6a809c8ded8
raspberrypisig <raspberrypisig@gmail.com>
2021-04-29 10:48:12 +1000

    Add files via upload

--- code/dmd3v2/dmd3.ino
+++ code/dmd3v2/dmd3.ino
@@ -1,5 +1,8 @@
 #include "dmd3.h"
 
+//#include "Droid_Sans_24.h"
+//#define DroidSansFont10x25 Droid_Sans_24
+
 //#include "digit_16.h"
 //#define DigitFont10x16 digit_16
 
@@ -10,6 +13,7 @@
 #define ArialFont16x16 Arial16x16
 
 DMD3 display(6,1);
+//DMD3 display(6,2);
 
 void setup()
 {
@@ -18,11 +22,15 @@ void setup()
   Serial.println("\n\nDraw buffer.....\n\n");
   
   char buf[1000];
+  
+  //display.setFont(DroidSansFont10x25);
   //display.setFont(DigitFont10x16);
   //display.setFont(ArialFont10x14);
   display.setFont(ArialFont16x16);
+  
   //for (int j=0; j<10; j++)
   //display.setPixel(3,j,DMD3::White);
+  
   display.drawChar(0,0,'a' ); 
   display.drawChar(16,0,'b');
   display.drawChar(32,0,'C');
@@ -31,7 +39,9 @@ void setup()
   display.drawChar(82,0,'3');
   //display.drawChar(0,0,'4');
   //display.drawChar(16,0,'5');
-  //display.drawChar(32,0,'6');  
+  //display.drawChar(32,0,'6'); 
+
+  //for (int i=0; i<32; i++) {
   for (int i=0; i<16; i++) {
     display.debugPixelLine(i, buf);
     Serial.println(buf);
