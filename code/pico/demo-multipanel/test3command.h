d96e98eab94b7e1c3067d371d441b15687b209a6
raspberrypisig <raspberrypisig@gmail.com>
2021-06-16 21:32:50 +1000

    Add files via upload

--- code/pico/demo-multipanel/test3command.h
+++ code/pico/demo-multipanel/test3command.h
@@ -4,11 +4,19 @@
 
 class Test3Command : public Command
 {
+  private:
+    unsigned long timeStart = 0UL;
+  
   public:
     void execute(DMD3 *canvas)  override {
-      clearSerialMonitor();
-      canvas->clear();
-      printCanvas(canvas);
-      canvas->update();
+      if ( millis() - timeStart > 5000) {  
+        clearSerialMonitor();
+        canvas->clear();
+        canvas->drawText(0,0, "HUMPTY");
+        canvas->drawText(0,16, "DUMPTY");
+        printCanvas(canvas);
+        canvas->update();
+        timeStart = millis();
+      }
     }
 };
