7e434f433c88b3ee6beca50f9c7ba3044ed0da1a
raspberrypisig <raspberrypisig@gmail.com>
2021-11-14 16:30:57 +1100

    Add files via upload

--- code/d1mini/games/src/routes.js
+++ code/d1mini/games/src/routes.js
@@ -1,7 +1,9 @@
 import Home from './routes/Home.svelte';
 import ImageUpload from './routes/ImageUpload.svelte';
+import ImageCreator from './routes/ImageCreator.svelte';
 
 export default {
     '/': Home,
     '/imageupload': ImageUpload,
+    '/imagecreator': ImageCreator
 };
