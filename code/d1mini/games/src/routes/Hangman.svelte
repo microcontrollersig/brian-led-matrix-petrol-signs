fc53c09c59f10698f33f7ddb1509480e38c9a7b4
raspberrypisig <raspberrypisig@gmail.com>
2021-11-20 07:43:35 +1100

    Add files via upload

---
+++ code/d1mini/games/src/routes/Hangman.svelte
@@ -0,0 +1,61 @@
+<script>
+    let categories = [
+        {
+            id: "countries",
+            name: "Countries",
+            url: "https://cdn.jsdelivr.net/gh/samayo/country-json/src/country-by-name.json",
+        },
+        {
+            id: "fruits",
+            name: "Fruits",
+            url: "https://cdn.jsdelivr.net/gh/raspberrypisig/corpora/custom/fruits.json",
+        },
+        {
+            id: "vegetables",
+            name: "Vegetables",
+            url: "https://cdn.jsdelivr.net/gh/raspberrypisig/corpora/custom/vegetables.json",
+        },
+        {
+            id: "animals",
+            name: "Animals",
+            url: "https://cdn.jsdelivr.net/gh/raspberrypisig/corpora/custom/animals.json",
+        },
+    ];
+
+    function selectCategory(id) {
+        const selectedCategories = categories.filter(
+            (category) => category.id === id
+        );
+        const selectedCategory = selectedCategories[0];
+        const selectedCategoryURL = selectedCategory.url;
+
+        // converted github link to jsdelivr CDN
+        fetch(selectedCategoryURL)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            });
+    }
+</script>
+
+<main>
+    <h1>Hangman</h1>
+    <section>
+        <h2>Categories</h2>
+        <ul>
+            {#each categories as category}
+                <li>
+                    <a
+                        class="categories"
+                        id={category.id}
+                        on:click={() => selectCategory(category.id)}
+                        >{category.name}</a
+                    >
+                </li>
+            {/each}
+        </ul>
+    </section>
+</main>
+
+<style lang="scss">
+</style>
--- code/d1mini/games/src/routes/Home.svelte
+++ code/d1mini/games/src/routes/Home.svelte
@@ -1,33 +1,32 @@
 <script>
-	
 </script>
 
 <main>
+    <section>
+        <h1>Apps</h1>
+        <ul>
+            <li><a href="#/imageupload">Image Upload</a></li>
+            <li><a href="#/imagecreator">Image Creator</a></li>
+            <li><a href="#/qrcode">QRCode</a></li>
+            <li><a href="#/tetristime">Tetris Time</a></li>
+            <li><a href="#/abbrev">Internet Abbrevations</a></li>
+            <li><a href="#/maze">Maze</a></li>
+            <li><a href="#/scoreboard">Scoreboard</a></li>
+        </ul>
+    </section>
 
-<section>
-<h1>Apps</h1>
-<ul>
-    <li><a href="#/imageupload">Image Upload</a></li>
-    <li><a href="#/imagecreator">Image Creator</a></li>
-    <li><a href="#qrcode">QRCode</a></li>
-    <li><a href="#tetristime">Tetris Time</a></li>
-    <li><a href="#abbrev">Internet Abbrevations</a></li>
-    <li><a href="#maze">Maze</a></li>
-    <li><a href="#scoreboard">Scoreboard</a></li>
-</ul>
-</section>
-
-<section>
-<h1>Games</h1>
-<ul>
-    <li><a href="#hangman">Hangman</a></li>
-    <li><a href="#dino">Dino</a></li>
-</ul>
-</section>	
-
-
+    <section>
+        <h1>Games</h1>
+        <ul>
+            <li><a href="#/hangman">Hangman</a></li>
+            <li><a href="#/dino">Dino</a></li>
+        </ul>
+    </section>
 </main>
 
 <style>
-
+    h1 {
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
 </style>
