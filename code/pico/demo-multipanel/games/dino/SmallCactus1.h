f36fd882f7399072dd76ad5bd1a12c3cc6704a06
raspberrypisig <raspberrypisig@gmail.com>
2021-11-26 11:54:40 +1100

    Add files via upload

---
+++ code/pico/demo-multipanel/games/dino/SmallCactus1.h
@@ -0,0 +1,10 @@
+#pragma once
+
+const static uint8_t SmallCactus1[] = {
+  0x00, 0x18, 0x18, 0x99, 0x99, 0xff, 0x7e, 0x18
+};
+const static int SMALLCACTUS1_LENGTH = 8;
+const static int SMALLCACTUS1_OFFSETX = 5;
+const static int SMALLCACTUS1_OFFSETY = 19;
+const static int SMALLCACTUS1_WIDTH = 8;
+const static int SMALLCACTUS1_HEIGHT = 8;
