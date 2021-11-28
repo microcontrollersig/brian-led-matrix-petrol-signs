eef00f6620c8356b6c369cdda804bc157fe6b236
raspberrypisig <raspberrypisig@gmail.com>
2021-11-28 20:24:04 +1100

    Add files via upload

--- code/d1mini/games/src/routes.js
+++ code/d1mini/games/src/routes.js
@@ -3,11 +3,15 @@ import ImageUpload from './routes/ImageUpload.svelte';
 import ImageCreator from './routes/ImageCreator.svelte';
 import Hangman from './routes/Hangman.svelte';
 import Dino from './routes/Dino.svelte';
+import RandomNumber from './routes/RandomNumber.svelte';
+import TetrisTime from './routes/TetrisTime.svelte';
 
 export default {
     '/': Home,
     '/imageupload': ImageUpload,
     '/imagecreator': ImageCreator,
     '/hangman': Hangman,
-    '/dino': Dino
+    '/dino': Dino,
+    '/randomnumber': RandomNumber,
+    '/tetristime': TetrisTime
 };
