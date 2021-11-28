459037ae3eb95694bfcc71bb4f7d422e362e5786
raspberrypisig <raspberrypisig@gmail.com>
2021-11-28 15:48:11 +1100

    Add files via upload

--- code/d1mini/games/src/routes.js
+++ code/d1mini/games/src/routes.js
@@ -2,10 +2,12 @@ import Home from './routes/Home.svelte';
 import ImageUpload from './routes/ImageUpload.svelte';
 import ImageCreator from './routes/ImageCreator.svelte';
 import Hangman from './routes/Hangman.svelte';
+import Dino from './routes/Dino.svelte';
 
 export default {
     '/': Home,
     '/imageupload': ImageUpload,
     '/imagecreator': ImageCreator,
-    '/hangman': Hangman
+    '/hangman': Hangman,
+    '/dino': Dino
 };
