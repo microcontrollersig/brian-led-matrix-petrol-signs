2a64eb3205bc594f38ba29071027a93566462f82
Raspberry Pi <raspberrypisig@gmail.com>
2021-11-29 18:58:58 +1100

    dino changes

--- code/d1mini/games/src/routes/Dino.svelte
+++ code/d1mini/games/src/routes/Dino.svelte
@@ -1,74 +1,92 @@
 <script>
-    import { onMount } from "svelte";
-    import { post_www_url_encoded, send_multiple } from "./sendcommmand.js";
+  import { onMount } from "svelte";
+  import { post_www_url_encoded, send_multiple } from "./sendcommmand.js";
 
-    function arrowDownPressed() {
-        //
-    }
+  let x = 0;
 
-    function arrowUpPressed() {
-        //
-    }
+  function arrowDownPressed() {
+    //
+  }
 
-    function spaceBarPressed() {
-        post_www_url_encoded({
-            command: "@",
-        });
+  function arrowUpPressed() {
+    //
+  }
 
-        const backgroundFull = [
-            {
-                dinocommand: "z",
-                subcommand: "d",
-                typeindex: 0,
-                objectindex: 0,
-                x: 0,
-                y: 0,
-            },
-            {
-                dinocommand: "z",
-                subcommand: "d",
-                typeIndex: 1,
-                objectindex: 0,
-                x: 80,
-                y: 16,
-            },
-            {
-                dinocommand: "z",
-                subcommand: "d",
-                typeindex: 2,
-                objectindex: 0,
-                x: 6,
-                y: 5,
-            },
-            {
-                dinocommand: "z",
-                subcommand: "u",
-                typeindex: -1,
-                objectindex: -1,
-                x: 0,
-                y: 0,
-            },
-        ];
-
-        send_multiple(backgroundFull);
-    }
-
-    onMount(() => {
-        document.addEventListener("keydown", function (event) {
-            const keycode = event.code;
-            if (keycode === "Space") {
-                spaceBarPressed();
-            } else if (keycode === "ArrowUp") {
-                arrowUpPressed();
-            } else if (keycode === "ArrowDown") {
-                arrowDownPressed();
-            }
-        });
+  function spaceBarPressed() {
+    post_www_url_encoded({
+      command: "@",
     });
+
+    const backgroundFull = [
+      {
+        dinocommand: "z",
+        subcommand: "d",
+        typeindex: 0,
+        objectindex: 0,
+        x: 0,
+        y: 0,
+      },
+      {
+        dinocommand: "z",
+        subcommand: "d",
+        typeIndex: 1,
+        objectindex: 0,
+        x: 80,
+        y: 16,
+      },
+      {
+        dinocommand: "z",
+        subcommand: "u",
+        typeindex: -1,
+        objectindex: -1,
+        x: 0,
+        y: 0,
+      },
+    ];
+
+    send_multiple(backgroundFull);
+
+    setInterval(() => {
+      const data = [
+        {
+          command: "z",
+          subcommand: "u",
+          arg1: -1,
+          arg2: -1,
+          x: x,
+          arg4: -1,
+        },
+        {
+          dinocommand: "z",
+          subcommand: "i",
+          typeindex: 2,
+          objectindex: 0,
+          x: 5,
+          y: 5,
+        },
+      ];
+      send_multiple(data);
+      x = x + 1;
+      if (x === 96 * 3) x = 0;
+    }, 200);
+  }
+
+  onMount(() => {
+    document.addEventListener("keydown", function (event) {
+      const keycode = event.code;
+      if (keycode === "Space") {
+        spaceBarPressed();
+      } else if (keycode === "ArrowUp") {
+        arrowUpPressed();
+      } else if (keycode === "ArrowDown") {
+        arrowDownPressed();
+      }
+    });
+  });
 </script>
 
 <main>
-    <h1>Dino</h1>
+  <h1>Dino</h1>
 </main>
 
 <style>
--- code/pico/demo-multipanel/gameDino.h
+++ code/pico/demo-multipanel/gameDino.h
@@ -3,88 +3,89 @@
 #include "command.h"
 #include "games/dino/DinoImages.h"
 
-class GameDino : public Command
-{
-  private:
+class GameDino : public Command {
+private:
+  unsigned long timeStart = 0UL;
+  char command;
+  int objectTypeIndex;
+  int objectIndex;
+  int x;
+  int y;
+  bool dirty = false;
 
-    unsigned long timeStart = 0UL;
-    char command;
-    int objectTypeIndex;
-    int objectIndex;
-    int x;
-    int y;
-    bool dirty = false;
+public:
+  DMD3 *virtualCanvas;
 
-  public:
-    DMD3 *virtualCanvas;
+  GameDino() { virtualCanvas = new DMD3(18, 2); }
+  void parseArgs(std::vector<std::string> args) override {
+    std::stringstream ss1, ss2, ss3, ss4, ss5;
+    ss1 << args.at(0);
+    ss1 >> command;
+    ss2 << args.at(1);
+    ss2 >> objectTypeIndex;
+    ss3 << args.at(2);
+    ss3 >> objectIndex;
+    ss4 << args.at(3);
+    ss4 >> x;
+    ss5 << args.at(4);
+    ss5 >> y;
+    dirty = true;
+  }
 
-    GameDino() {
-      virtualCanvas = new DMD3(18, 2);
-    }
-    void parseArgs(std::vector<std::string> args) override {
-      std::stringstream ss1, ss2, ss3, ss4, ss5;
-      ss1 << args.at(0);
-      ss1 >> command;
-      ss2 << args.at(1);
-      ss2 >> objectTypeIndex;
-      ss3 << args.at(2);
-      ss3 >> objectIndex;
-      ss4 << args.at(3);
-      ss4 >> x;
-      ss5 << args.at(4);
-      ss5 >> y;
-      dirty = true;
-    }
+  void drawDinoGameObject(DMD3 *dmdObject) {
+    int k = 0;
+    int l = 0;
 
-    void drawDinoGameObject() {
-      int k = 0;
-      int l = 0;
+    uint8_t *data =
+        (uint8_t *)DinoObjectTypes[objectTypeIndex][objectIndex].obj;
+    int length = DinoObjectTypes[objectTypeIndex][objectIndex].length;
+    int width = DinoObjectTypes[objectTypeIndex][objectIndex].width;
 
-      uint8_t *data = (uint8_t *) DinoObjectTypes[objectTypeIndex][objectIndex].obj;
-      int length = DinoObjectTypes[objectTypeIndex][objectIndex].length;
-      int width =  DinoObjectTypes[objectTypeIndex][objectIndex].width;
+    int bytesPerRow = width / 8;
 
+    for (int i = 0; i < length; i++) {
 
-      int bytesPerRow = width / 8;
-
-      for (int i = 0; i < length; i++) {
-
-        uint8_t val = data[i];
-        for (int j = 7; j >= 0; j--) {
-          uint8_t pixel = (1 << j) & val;
-          if (pixel != 0) {
-            virtualCanvas->setPixel(7 - j + x + k, y + l);
-          }
+      uint8_t val = data[i];
+      for (int j = 7; j >= 0; j--) {
+        uint8_t pixel = (1 << j) & val;
+        if (pixel != 0) {
+          dmdObject->setPixel(7 - j + x + k, y + l);
         }
-
-        if ( ((i + 1) % bytesPerRow) == 0)
-        {
-          k = 0;
-          l++;
-        }
-
-        else
-          k = k + 8;
       }
 
-    }
-
-    void execute(DMD3 *canvas)  override {
-      if (dirty) {
-        switch (command) {
-          case 'd':
-            drawDinoGameObject();
-            break;
-
-          case 'u':
-            clearSerialMonitor();
-            virtualCanvas->copy(x,0,96,32,canvas,0,0);
-            printCanvas(canvas);
-            canvas->update();
-            break;
-        }
-
-        dirty = false;
+      if (((i + 1) % bytesPerRow) == 0) {
+        k = 0;
+        l++;
       }
+
+      else
+        k = k + 8;
     }
+  }
+
+  void execute(DMD3 *canvas) override {
+    if (dirty) {
+      switch (command) {
+      case 'd':
+        drawDinoGameObject(virtualCanvas);
+        break;
+
+      case 'i':
+        clearSerialMonitor();
+        drawDinoGameObject(canvas);
+        printCanvas(canvas);
+        canvas->update();
+        break;
+
+      case 'u':
+        clearSerialMonitor();
+        virtualCanvas->copy(x, 0, 96, 32, canvas, 0, 0);
+        printCanvas(canvas);
+        canvas->update();
+        break;
+      }
+
+      dirty = false;
+    }
+  }
 };
