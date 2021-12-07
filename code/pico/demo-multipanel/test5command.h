5eeb3efea6f99dac04006838008bcb86c18cf042
Raspberry Pi <raspberrypisig@gmail.com>
2021-12-07 17:14:13 +1100

    panel size accomodation

--- code/pico/demo-multipanel/test5command.h
+++ code/pico/demo-multipanel/test5command.h
@@ -5,45 +5,57 @@
 
 
 class Test5Command : public Command
-{  
+{
   private:
     TestMovingMode mode = TestMovingMode::HORIZONTAL;
     int i = 0;
     int j = 0;
     unsigned long timeStart = 0UL;
-    int panelX = 80;
-    int panelY = 16;
-  
+    int panelX = 0;
+    int panelY = 0;
+    int panelXInitial = 0;
+    int panelYInitial = 0;
+    bool initialized = false;
+
   public:
     void execute(DMD3 *canvas)  override {
+      if (!initialized) {
+        int width = canvas->width();
+        int height = canvas->height();
+        panelX = width - 16;
+        panelY = height - 16;
+        panelXInitial = panelX;
+        panelYInitial = panelY;
+        initialized = true;
+      }
+
       if ( millis() - timeStart > 3000) {
         clearSerialMonitor();
         canvas->clear();
 
 
-        for (int i=0; i< 16; i++) {
-          for (int j=0; j< 16; j++) {
-         canvas->setPixel(panelX + i, panelY + j);  
+        for (int i = 0; i < 16; i++) {
+          for (int j = 0; j < 16; j++) {
+            canvas->setPixel(panelX + i, panelY + j);
+          }
         }
-        }
-  
+
 
         panelX = panelX - 16;
         if (panelX == -16) {
-          panelX = 80;
-          if (panelY == 16) {
-            panelY = 0;
+          panelX = panelXInitial;
+          panelY = panelY - 16;
+          if (panelY == -16) {
+            panelY = panelYInitial;
           }
-          else if (panelY == 0) {
-            panelY = 16;
-          }
-          
+
+
         }
-       
+
         printCanvas(canvas);
-        canvas->update();      
+        canvas->update();
         timeStart = millis();
       }
     }
-        
+
 };
